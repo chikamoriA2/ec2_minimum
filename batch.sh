2e38d359417637c21095e4d1dc4aec495e893a16
EC2 Default User <ec2-user@ip-172-31-28-152.ap-northeast-1.compute.internal>
2025-01-21 09:04:40 +0000

    first commit

---
+++ batch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+sh Anaconda3-2024.10-1-Linux-x86_64.sh -b
+export PATH=/home/ec2-user/anaconda3/bin:$PATH
+
+
+
+
